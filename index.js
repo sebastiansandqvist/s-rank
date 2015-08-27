816f8c1882a0cadb5d7a43ffb13ccc880b88f12c
Sebastian Sandqvist <seb@sparque.me>
2015-08-27 12:15:33 -0700

    initial work

---
+++ index.js
@@ -0,0 +1,15 @@
+'use strict';
+
+function rank(votes, timestamp) {
+
+	var order = Math.log(Math.max(votes, 1)); // 0 votes === 1 vote
+	var delta = timestamp - rank.epoch;
+
+	return order + (delta / rank.baseTime);
+
+}
+
+rank.epoch = new Date(1970,1,1).getTime();
+rank.baseTime = 12 * 60 * 60 * 1000; // 12 hours
+
+module.exports = rank;
